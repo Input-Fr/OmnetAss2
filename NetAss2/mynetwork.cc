3fdf477f842b2bc4e26c85476b2e6b7dd6ae5487
c23cs1002 <161812194+c23cs1002@users.noreply.github.com>
2024-04-03 18:59:41 +0530

    Update mynetwork.cc

--- NetAss2/mynetwork.cc
+++ NetAss2/mynetwork.cc
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <random>
 #include <fstream> // Include the header for file input/output operations
+#include <unistd.h> // for sleep()
 
 using namespace omnetpp;
 
@@ -65,7 +66,37 @@ std::vector<std::list<int>> divideList(const std::list<int>& myList, int m) {
     return dividedParts;
 }
 
-std::vector<int> generateAndShuffle(int n) {
+std::vector<int> generateAndShuffle(int n, std::list<std::string> forbidden) {
+    if (forbidden.size() != 0)
+    {
+        // Create a vector containing integers from 0 to n
+        std::vector<int> numbers(n);
+        std::iota(numbers.begin(), numbers.end(), 0);
+
+        // Shuffle the vector
+        std::random_device rd;
+        std::mt19937 g(rd());
+        std::shuffle(numbers.begin(), numbers.end(), g);
+        for (const auto& str : forbidden)
+        {
+            if (str != "")
+            {
+                std::string numericPart;
+                for (char c : str) {
+                    if (isdigit(c)) {
+                        numericPart += c;
+                    }
+                }
+
+                int intValue = std::stoi(numericPart);
+                numbers.erase(std::remove(numbers.begin(), numbers.end(), intValue), numbers.end());
+            }
+
+        }
+
+        return numbers;
+    }
+
     // Create a vector containing integers from 0 to n
     std::vector<int> numbers(n);
     std::iota(numbers.begin(), numbers.end(), 0);
@@ -79,6 +110,8 @@ std::vector<int> generateAndShuffle(int n) {
 
     return numbers;
 }
+
+
 
 void Node::initialize()
 {
@@ -107,7 +140,7 @@ void Node::initialize()
             n = entry.value;
     }
 
-    shuffledNumbers = generateAndShuffle(n);
+    shuffledNumbers = generateAndShuffle(n,defectiveServers);
 
     // Keep only the first n/2 + 1 elements
     shuffledNumbers.resize(n / 2 + 1);
@@ -142,13 +175,11 @@ void Node::initialize()
             for(int i = 0; i < n/2+1; i++)
             {
                 std::string servName = "server" + std::to_string(shuffledNumbers[i]);
-                std::cout << "retrieved name : " << servName << std::endl;
 
                 hostConnections[servName.c_str()] = getParentModule()->getSubmodule(servName.c_str());
                 if (hostConnections[servName.c_str()] == nullptr)
                 {
                     servName = "serverMalicious" + std::to_string(shuffledNumbers[i]);
-                    std::cout << "retrieved name : " << servName << std::endl;
                     hostConnections[servName.c_str()] = getParentModule()->getSubmodule(servName.c_str());
 
                 }
@@ -159,8 +190,6 @@ void Node::initialize()
                 // Iterate through gates of the client module
                 for (int j = 0; j < gateSize("gate$o"); j++) {
                     cGate *gate = this->gate("gate$o", j);
-                    std::cout << "retrieved gate : " << gate->getPathEndGate()->getOwnerModule() << std::endl;
-                    std::cout << "wanted gate : " << serverModule << std::endl;
 
                     if (gate->getType() == cGate::OUTPUT && gate->getPathEndGate()->getOwnerModule() == serverModule) {
                         serverGate = gate;
@@ -183,11 +212,11 @@ void Node::initialize()
         // i am a server
     }
 
-
 }
 
 void Node::handleMessage(cMessage *msg)
 {
+    std::ofstream outputFile("example.txt", std::ios::app);
     // The handleMessage() method is called whenever a message arrives at the module.
 
     // am I a server ?
@@ -212,6 +241,7 @@ void Node::handleMessage(cMessage *msg)
             // Je suis un farfadet rempli de malices
             std::string str = std::to_string(subList[0]);
             answer = new cMessage(str.c_str());
+
         }
         else
         {
@@ -234,19 +264,99 @@ void Node::handleMessage(cMessage *msg)
 
         // Send the message back to the sender module
         send(answer, serverGate);
+
     }
-    else
+    else // i am a client
     {
         std::string msgContent = msg->getName();
         if (msgContent.find(":") != std::string::npos) {
             // Le message est une liste de serveurs défectueux
+            size_t lastColonPos = msgContent.find_last_of(':');
+            msgContent = msgContent.substr(lastColonPos + 1);
             std::stringstream ss(msgContent);
             std::string serverInfo;
 
             while (getline(ss, serverInfo, ',')) {
-                // Store defective Servers in the list
-                defectiveServers.push_back(serverInfo);
+                // Store defective Servers in the list only if it is not already in
+                auto it = std::find(defectiveServers.begin(), defectiveServers.end(), serverInfo);
+                if (it == defectiveServers.end())
+                {
+                    defectiveServers.push_back(serverInfo);
+                }
+
             }
+
+            // additional code, next round
+            EV << "New malicious server list of " << this->gate("gate$i",0)->getPathEndGate()->getOwnerModule()->getName() << " : " << endl;
+            for (const auto& str : defectiveServers) {
+                    EV << str << endl;
+            }
+
+            shuffledNumbers = generateAndShuffle(n,defectiveServers);
+
+            // Keep only the first n/2 + 1 elements
+            shuffledNumbers.resize(n / 2 + 1);
+            //TODO maybe change the value of the totalServers
+            totalServers = n / 2 + 1;
+
+            std::list<int> myList = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+            int m = n; // nb of server to begin
+            int subSize = myList.size() / m;
+            while (subSize < 2)
+            {
+                m--;
+                subSize = myList.size() / m;
+            }
+            std::vector<std::list<int>> dividedParts = divideList(myList, m);
+
+            // for every sublist
+            for(int j = 0; j < m; j++)
+            {
+                std::list<int> intArray = dividedParts[j];
+                std::string messageContent = "";
+
+                for (const auto& elem : intArray)
+                {
+                    messageContent += std::to_string(elem);
+                    messageContent += ",";
+                }
+                messageContent.erase(messageContent.size() - 1); // erase the last character
+                // for every server
+                for(int i = 0; i < n/2+1; i++)
+                {
+                    std::string servName = "server" + std::to_string(shuffledNumbers[i]);
+
+                    hostConnections[servName.c_str()] = getParentModule()->getSubmodule(servName.c_str());
+                    if (hostConnections[servName.c_str()] == nullptr)
+                    {
+                        servName = "serverMalicious" + std::to_string(shuffledNumbers[i]);
+                        hostConnections[servName.c_str()] = getParentModule()->getSubmodule(servName.c_str());
+
+                    }
+                    // find the right gate and iterate through gates of the client module
+                    cModule *serverModule = hostConnections[servName.c_str()];
+                    cGate *serverGate = nullptr;
+
+                    // Iterate through gates of the client module
+                    for (int j = 0; j < gateSize("gate$o"); j++) {
+                        cGate *gate = this->gate("gate$o", j);
+
+                        if (gate->getType() == cGate::OUTPUT && gate->getPathEndGate()->getOwnerModule() == serverModule) {
+                            serverGate = gate;
+                            break;
+                        }
+                    }
+
+
+                    // Send a message to the server
+
+                    cMessage *msg = new cMessage(messageContent.c_str());
+                    send(msg, serverGate);
+
+                }
+            }
+
+
         }
         else {
             int receivedMax = std::stoi(msg->getName());
@@ -254,11 +364,9 @@ void Node::handleMessage(cMessage *msg)
             maxValues.push_back(std::make_pair(receivedMax, senderName));
 
             receivedResponses++;
-            std::cout << "Response received: " << receivedResponses << std::endl;
 
             // Verify if all the responses has been received
             if (receivedResponses == totalServers) {
-                std::cout << "All maximum values have been received." << std::endl;
 
                 //TODO get the real Ip address
                 //cModule * host = getContainingNode(this);
@@ -280,9 +388,12 @@ void Node::handleMessage(cMessage *msg)
                     }
                 }
 
+                std::cout << "clientID : " << getId << " - " << "Majority from subtask : " << majorityValue << std::endl();
+                outputFile << "clientID : " << getId << " - " << "Majority from subtask : " << majorityValue << std::endl();
                 std::stringstream s;
                 for (const auto &maxVal: maxValues) {
                     if (maxVal.first != majorityValue) {
+                        defectiveServers.push_back(maxVal.second);
                         s << maxVal.second << ",";
                     }
                 }
@@ -297,25 +408,14 @@ void Node::handleMessage(cMessage *msg)
                         currentTime.dbl());
 
                 cModule *networkModule = getParentModule();
-                std::string ipAddress = this->gate("gate$i",0)->getPathEndGate()->getOwnerModule()->getName();  // Default Value
-                std::cout << ipAddress << std::endl;
-                if (networkModule) {
-                    std::cout << "NetworkModule is not Null" << std::endl;
-                    cModule *ipModule = networkModule->getSubmodule("ipv4");
+                std::string ipAddress = std::to_string(getId());
 
-                    if (ipModule) {
-                        ipAddress = ipModule->par("ipAddress").stringValue();
-                    }
-                }
-
-                ss << timestamp << ":" << ipAddress << ":" << nonMajorityServers
-                   << ",";
+                ss << timestamp << ":" << ipAddress << ":" << nonMajorityServers << ",";
                 std::string maliciousServersInfo = ss.str();
                 if (!maliciousServersInfo.empty()) {
                     maliciousServersInfo.pop_back();
                 }
 
-                std::cout << maliciousServersInfo << std::endl;
 
                 for (int j = 0; j < gateSize("gate$o"); j++) {
                     cGate *gate = this->gate("gate$o", j);
@@ -328,8 +428,11 @@ void Node::handleMessage(cMessage *msg)
                     }
                 }
 
-                // additional code
+
+
             }
+
         }
     }
+    outputFile.close();
 }
